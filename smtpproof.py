41cfe6d5722259e8744703c53c4247c18f96bca4
jz36 <ivan.zykov95@yandex.ru>
2016-05-12 12:21:14 +0500

    Create smtpproof for send mail, and still work on GUI

---
+++ smtpproof.py
@@ -0,0 +1,29 @@
+from smtplib import SMTP_SSL
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders as Encoders
+import os
+
+filepath = "/home/jz36/Документы/chess/bBishop.png"
+basename = os.path.basename(filepath)
+address = "neo@biksileev.ru"
+
+# Compose attachment
+part = MIMEBase('application', "octet-stream")
+part.set_payload(open(filepath,"rb").read() )
+Encoders.encode_base64(part)
+part.add_header('Content-Disposition', 'attachment; filename="%s"' % basename)
+
+# Compose message
+msg = MIMEMultipart()
+msg['From'] = address
+msg['To'] = 'texpomruu@yandex.ru'
+
+msg.attach(part)
+
+# Send mail
+smtp = SMTP_SSL()
+smtp.connect('smtp.yandex.ru')
+smtp.login(address, 'rjcjq12utybq')
+smtp.sendmail(address, 'texpomruu@yandex.ru', msg.as_string())
+smtp.quit()
